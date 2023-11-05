59247de109375249fb1cab38323f6bd166f7b6d0
unknown <121388798+Chan-Dong-Jun@users.noreply.github.com>
2023-11-05 21:54:08 +0800

    -

---
+++ cpp learn/visual.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // for std::cout
+
+int main()
+{
+    int x{ 5 };
+    std::cout << "x is equal to: " << x << '\n'; // Using '\n' standalone
+    std::cout << "And that's all, folks!\n"; // Using '\n' embedded into a double-quoted piece of text (note: no single quotes when used this way)
+    return 0;
+}
