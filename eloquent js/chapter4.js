a6841785992c45a675671e69f9ea6e0286f94847
unknown <121388798+Chan-Dong-Jun@users.noreply.github.com>
2023-11-10 12:26:33 +0800

    -

--- eloquent js/chapter4.js
+++ eloquent js/chapter4.js
@@ -67,8 +67,74 @@ function every(array, test) {
 }
 
 function everySome(array, test) {
-    return array.some(() => test)
+    return !(array.some(element => !test(element)))
 }
+console.log(every([1, 3, 5], n => n < 10));
+
 console.log(everySome([], n => n < 10));
 
 console.log(everySome([2, 4, 16], n => n < 10));
+
+
+/*
+Dominant writing direction
+
+Write a function that computes the dominant writing direction in a string of text. Remember that each script object has a direction property that can be "ltr" (left to right), "rtl" (right to left), or "ttb" (top to bottom).
+
+The dominant direction is the direction of a majority of the characters that have a script associated with them. The characterScript and countBy functions defined earlier in the chapter are probably useful here.
+
+function dominantDirection(text) {
+  // Your code here.
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
+*/
+
+
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+      let name = groupName(item);
+      let known = counts.findIndex(c => c.name == name);
+      if (known == -1) {
+        counts.push({name, count: 1});
+      } else {
+        counts[known].count++;
+      }
+    }
+    return counts;
+  }
+  console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+
+function characterScript(code) {
+for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+    return code >= from && code < to;
+    })) {
+    return script;
+    }
+}
+return null;
+}
+  
+
+
+function textScripts(text) {
+    let scripts = countBy(text, char => {
+      let script = characterScript(char.codePointAt(0));
+      return script ? script.name : "none";
+    }).filter(({name}) => name != "none");
+  
+    let total = scripts.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found";
+  
+    return scripts.map(({name, count}) => {
+      return `${Math.round(count * 100 / total)}% ${name}`;
+    }).join(", ");
+  }
+
+let script = '英国的狗说"woof", 俄罗斯的狗说"тяв"';
+console.log(script.textScripts);
